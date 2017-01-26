1fb5c32db4a3c09354866d37de6d4bd99591e1d5
Tyler <tyler@eeosk.com>
2017-01-26 15:17:58 -0800

    Update server port

--- server.js
+++ server.js
@@ -4,15 +4,7 @@ let express     = require('express')
 ,   serveStatic = require('serve-static')
 ,   path        = require('path')
 ,   app = express()
-
-switch (process.env.NODE_ENV) {
-  case 'production':
-  case 'staging':
-    break
-  default:
-    process.env.NODE_ENV = 'development'
-    process.env.PORT = 3000
-}
+,   port = process.env.PORT || 3000
 
 app.get('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, '') + '/index.html')
@@ -20,6 +12,6 @@ app.get('/', (req, res, next) => {
 
 app.use(serveStatic(path.join(__dirname, '')))
 
-app.listen(process.env.PORT, () => {
-  console.log('Example app listening on port ' + process.env.PORT)
+app.listen(port, () => {
+  console.log('App listening on port ' + port)
 })
